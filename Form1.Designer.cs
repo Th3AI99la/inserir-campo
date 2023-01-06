687500ae5f748ab3c7b2d7ffd38692d98424a7fb
Thalles Alves <104025889+Th3AI99la@users.noreply.github.com>
2023-01-06 15:20:54 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace tabela_de_busca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridNome = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GridNome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridNome
+            // 
+            this.GridNome.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GridNome.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridNome.Location = new System.Drawing.Point(161, 177);
+            this.GridNome.Name = "GridNome";
+            this.GridNome.Size = new System.Drawing.Size(306, 256);
+            this.GridNome.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(227, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textNome
+            // 
+            this.textNome.Location = new System.Drawing.Point(190, 94);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(245, 20);
+            this.textNome.TabIndex = 2;
+            this.textNome.TextChanged += new System.EventHandler(this.textNome_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(617, 527);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GridNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GridNome)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GridNome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabela_de_busca
+{
+    public partial class Form1 : Form
+    {
+        public List <Dados> dados { get; set;  }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dados = new List<Dados>();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dados.Add(new Dados { Nome = textNome.Text });
+            GridNome.DataSource = null;
+            GridNome.DataSource = dados;
+            textNome.Text = "";
+
+
+           
+      
+
+        }
+
+        public class Dados
+        {
+            public string Nome { get; set; }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
